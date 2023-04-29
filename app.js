cca44c638cb3f5bcea9432f42a2970bb5ea26eca
CyberLynx1021 <abdulazizali234@gmail.com>
2023-04-29 18:58:43 -0500

    javascript crash course completed  min

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-let users = [
-{ 
-        username: "David",
-        email: "david@frontend",
-        password: "test123",
-        subcriptionsStatus: "VIP",
-        discordId: "Mitri#001",
-        lessonsCompleted: [0, 1],
-},
-{
-    username: "Mitri",
-    email: "mitri@frontend",
-    subcriptionsStatus: "vip",
-    discordId: "Mitri#001",
-    lessonsCompleted: [0,1,2,3],
+function toggleDarkMode(){
+document.querySelector('body').classList.toggle("dark-theme")
 }
-
-]
-console.log(users[0].lessonsCompleted.map(elem => elem * 2));
