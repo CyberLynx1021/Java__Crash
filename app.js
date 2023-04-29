b79460f551b29c81ca88ac5f570736a79adddb1a
CyberLynx1021 <abdulazizali234@gmail.com>
2023-04-28 22:38:31 -0500

    javascript crash course 2hr 11 min

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-let arr = [20, 30, 40, 50, 60]
-arr.push(200)
-console.log(arr)
+let users = [
+{ 
+        username: "David",
+        email: "david@frontend",
+        password: "test123",
+        subcriptionsStatus: "VIP",
+        discordId: "Mitri#001",
+        lessonsCompleted: [0, 1],
+},
+{
+    username: "Mitri",
+    email: "mitri@frontend",
+    subcriptionsStatus: "vip",
+    discordId: "Mitri#001",
+    lessonsCompleted: [0,1,2,3],
+}
+
+]
+console.log(users[0].lessonsCompleted.map(elem => elem * 2));
