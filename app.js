8f12cd3fabac4b0be664a2fde57b613c3f12c7bb
CyberLynx1021 <abdulazizali234@gmail.com>
2023-05-04 22:33:57 -0500

    Promises 8:45 sec

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
 const emailRef = document.querySelector(".email");
 console.log(emailRef)
-// 1 Then
+// 1 Then second
 fetch("https://jsonplaceholder.typicode.com/users/1").then((response) =>{
     response.json().then(data => {
         console.log(data)
