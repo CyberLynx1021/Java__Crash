3b85a1de39c8f58787ba80823675ae8a476fdde1
CyberLynx1021 <abdulazizali234@gmail.com>
2023-05-05 01:22:28 -0500

    Promises finish

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
-// console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
-const emailRef = document.querySelector(".email");
+const statusRef = document.querySelector('.status')
+const videoRef = document.querySelector('.video')
 
-// 1 Then second
-// fetch("https://jsonplaceholder.typicode.com/users/1")
-//     .then((response) =>{
-//     return response.json()
-//     }).then(data =>{
-//         console.log(data)
-//         emailRef.innerHTML = data.email
-//     })
-
-// 2. Async/Await
-async function main(){
-const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
-console.log(response.json())
+function getSubscriptionStatus() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        resolve("undefined")
+        }, 2000);
+    })
 }
 
-main();
+ function getVideo(subscriptionStatus){
+    return new Promise((resolve, reject) =>{
+        if (subscriptionStatus === "VIP"){
+            resolve("show video")
+    }
+    else if(subscriptionStatus === "FREE")
+    resolve("show trailer")
+
+    else (subscriptionStatus)
+    reject("no video")
+})
+ }
+
+ async function main(){
+ const status = await getSubscriptionStatus();
+ statusRef.innerHTML = status;
+ try{
+    console.log(await getVideo(status))
+}
+catch(e){
+    console.log (e) 
+    videoRef.innerHTML = e;
+}
+ }
+
+ main();
