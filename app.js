a540758e4973fa58f05c0a423f5b286a2d88052f
CyberLynx1021 <abdulazizali234@gmail.com>
2023-05-07 18:56:48 -0500

    JS medium 35

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-// create function that takes 2 strings. If 1st string is
-// equal to "LOGGED_IN" or second string = Subscribed return
-// true otherwise return false isLoggedInAndSubscribed("LOGGED_IN",SUBSCRIBED) -> true
-
-
-
-function isLoggedInAndSubscribed(Logged_In, Subscribed){
-if (Logged_In ==="LOGGED_IN" || Subscribed === "SUBSCRIBED")
-return true
-else 
-return false
+// Given a array of elements, return the same length array
+//  filled with 0's convertToZeros([5,100,0]) -->
+function convertToZeros(arr){
+    let newArr = []
+    for (let i = 0; i < arr.length;  ++i) {
+        newArr[i] = 0
+ }
+   return newArr;
 }
-
-console.log(isLoggedInAndSubscribed("LOGGED_IN","SUBSCRIBED" ))
-
+console.log(convertToZeros([5,100,0]))
