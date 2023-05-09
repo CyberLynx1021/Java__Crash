fbf0bf1bbd7ca6c96fabf3948007e294411a823a
CyberLynx1021 <abdulazizali234@gmail.com>
2023-05-09 00:55:37 -0500

    JS hard question finished

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-// Given an array of truthy and falsy values return same
-// array of elements into its boolean value.
-// converToBoolean([500,0,"David","",[])
-function converToBoolean(arr){
-arrNew = []
-// Makes every element either truth or falsy depending on value
-return arr.map(elem => !!elem)
-  }
-
-console.log(converToBoolean([500,0,0,"David","",]))
+// call this API "https//jsonplaceholder.typicode.com/todos"
+// and return all posts by any given user id.
+async function firstSixIncomplete(userId) {
+    // getting promise from fetch API
+   const promise = await fetch("https://jsonplaceholder.typicode.com/todos");
+    // converting from backend lan to json wait for it
+   const result = await promise.json()
+// result filter element if userId is = to parameter userId return 
+// ! saying if its not true then slice it 6 times
+   const incompleTasks = result.filter(elem => !elem.completed).slice(0,6)
+console.log(incompleTasks)
+}
+ 
+firstSixIncomplete(6);
